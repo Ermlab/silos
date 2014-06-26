c15dd4473914137bbe7e7e5d5ed4611d09db6876
radekbor <radoslaw.borowiecki@gmail.com>
2014-06-26 13:14:27 +0200

    modify api example

--- Api/Example/Example/Program.cs
+++ Api/Example/Example/Program.cs
@@ -7,7 +7,17 @@ namespace Example
 		public static void Main (string[] args)
 		{
 			Silos api = new Silos ("http://localhost", "3000", "9NZvdpkbwP9FjsNZYH7egzQb7oyqbujER2C568i9cocviTHgW8Y4QEs5C9oHyFAX");
-			Console.WriteLine (api.Debug ("LogName"," LogBody","LogThread"));
+			String respond=api.Debug ("LogName"," LogBody","LogThread");
+			/*
+			 * Rest of api logging function 
+			 * each function has the same parameters
+			 * api.Information
+			 * api.Warnings
+			 * api.Error
+			 * api.Fatal
+			 * 
+			 */
+			Console.WriteLine (respond);
 			Console.ReadLine ();
 		}
 	}
