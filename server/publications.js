feea3241e3ccb34621ad5fe32ad3d04b17c54bfe
radekbor <radoslaw.borowiecki@gmail.com>
2014-06-30 13:36:55 +0200

    fix merge bug

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,10 @@
 Meteor.publish('myLogBooks', function(authorId) {
   return LogBooks.find({AuthorID: authorId});
 });
+Meteor.publish('logBooks', function(authorId) {
+    return LogBooks.find({AuthorID: authorId});
+});
+
 Meteor.publish('logs', function(LogBookId) {
     return Logs.find({LogBookID: LogBookId});
 });
