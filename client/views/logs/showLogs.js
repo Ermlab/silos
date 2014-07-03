f18a5e31c9e3119e81c5d184af21cef3b19ddc32
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2014-07-03 14:24:46 +0200

    Exporting to .csv [fixed]

--- client/views/logs/showLogs.js
+++ client/views/logs/showLogs.js
@@ -86,7 +86,11 @@ Template.showLogs.events({
         $(e.target).find('[level=Level]').val();
     },
     ownPost: function() {
-        return this.AuthorID == Meteor.userId(); }
+        return this.AuthorID == Meteor.userId(); },
+    'click .download': function (e) {
+        csv = json2csv(Logs.find().fetch(), true, true)
+        e.target.href = "data:text/csv;charset=utf-8," + escape(csv)
+        e.target.download = "logs.csv"; }
 });
 
 Template.showLogs.rendered=function()
@@ -111,5 +115,74 @@ Template.showLogs.events({
         Router.go(path)
     }
 })
+function json2csv(objArray, headers, showHeaders) {
 
+    if( typeof headers == "boolean" ){
+        showHeaders = headers;
+        headers = null;
+    }
+
+    var itens = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+
+    //separate fields
+    var fields = {};
+
+    if( !headers ){
+        for(var i = 0; i < itens.length; i++){
+            for(var prop in itens[i]){
+                if( !fields[prop] ){
+                    fields[prop] = [];
+                }
+            }
+        }
+    }else{
+        headers.forEach(function(header){ fields[header] = []; });
+    }
+
+
+    //getting data
+
+    for(var i = 0; i < itens.length; i++){
+        for(var prop in fields){
+            if( typeof itens[i][prop] != "undefined" ){
+                fields[prop].push( itens[i][prop] );
+            }else{
+                fields[prop].push( "" );
+            }
+        }
+    }
+
+    //make the csv
+
+    var csvLines = [];
+
+    if( showHeaders ){
+
+        var lineFields = [];
+
+        for(var prop in fields){
+            lineFields.push(  prop );
+        }
+
+        var line = lineFields.join(", ");
+        csvLines.push(line);
+
+    }
+
+    for(var i = 0; i < itens.length; i++){
+
+        var lineFields = [];
+
+        for(var prop in fields){
+            lineFields.push(  fields[prop][i] );
+        }
+
+        var line = lineFields.join(", ");
+        csvLines.push(line);
+    }
+
+    var csvStr = "sep=,\n" + csvLines.join("\n");
+
+    return csvStr;
+}
 
