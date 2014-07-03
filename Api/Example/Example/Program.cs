c0dd0c51dfd2c5723dc5b4c13b3bb9e109eea0a8
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2014-07-03 13:36:36 +0200

    Exporting logs to .csv

--- Api/Example/Example/Program.cs
+++ Api/Example/Example/Program.cs
@@ -6,7 +6,7 @@ namespace Example
 	{
 		public static void Main (string[] args)
 		{
-			Silos api = new Silos ("http://localhost", "3000", "6mMz9KcvF5N9HKnTYpia7gS4rcYk5o6u6Hzgdrv4wohLdzR5JisDWM5T5EeLSYoj","LogThread");
+			Silos api = new Silos ("http://localhost", "3000", "3uP2NBQfQZuRkRSFqncttRZepjzvbke9KPWKrP8KSG7CDinXwHgfrwDeoJ3iQ3eL","LogThread");
 			String respond=api.Debug ("inny"," Tresc","tag2 tag4");
 			/*
 			 * Rest of api logging function 
