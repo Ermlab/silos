8f0ccbb0d4c0b5ca24b5ecfb19d12e2e66a24d34
radekbor <radoslaw.borowiecki@gmail.com>
2014-07-12 17:01:45 +0200

    create python api

---
+++ Api/Python/Example.py
@@ -0,0 +1,4 @@
+import Library.Silos
+obj=Library.Silos.Api("http://localhost", "3000","3zHcA8g2rktr5QNTNN7bizYLmCioesZC73ieAQ8DTdGJ5D23ZGZwNF2CTJvu7uwX")
+
+print (obj.debug())
---
+++ Api/Python/Library/Silos.py
@@ -0,0 +1,20 @@
+import http.client, urllib.parse
+
+class Api:
+    def __init__(self,Host,Port,Key):
+        self.host=Host
+        self.port=Port
+        self.key=Key
+    def _send(self):
+
+        data=urllib.parse.urlencode({'LogMsg': "messege","Pole2":"Pole32"})
+        headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = http.client.HTTPConnection('{0}/api/call/{2}'.format(self.host,self.port,self.key),self.port)
+        conn.request("POST", "", data,headers)
+                                          
+
+
+        
+        return data
+    def debug(self):
+        return self._send()
