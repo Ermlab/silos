c521b90520a79182c260ea8b6ad77fc20533e67c
radekbor <radoslaw.borowiecki@gmail.com>
2014-07-14 11:53:20 +0200

    filtering selected from list, fix clean logBook

--- Api/Python/Example.py
+++ Api/Python/Example.py
@@ -1,5 +1,5 @@
 import Library.Silos
-obj=Library.Silos.Api("http://localhost","3000","t5zPSbLQM63XtF3dA7fe7pbnW3SdFFtYKkwGnMkpBrFQz9zxdzASxCWQDZRNzsTF")
+obj=Library.Silos.Api("http://localhost","3000","poKvbkvJp8ByfbYhfWFjAMsZQS2stoSNE3FPpmDFywCeQ2pHjbWqKifEKFve6L4E")
 obj.setField("Pole 23", "2")
 obj.setField("Pole 23", "4")
 obj.debug("inne")
