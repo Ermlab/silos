1323e7baee1f55cb8617f4cdbccf3e8d8793680e
Przemysław Górecki <gorecki@ermlab.com>
2014-09-03 09:18:38 +0200

    Added autoscroll feature and table resize with sticky headers

--- client/main.js
+++ client/main.js
@@ -1,4 +1,33 @@
+var loggers = {};
+
 Meteor.startup(function () {
     Meteor.subscribe('logBooks');
     Meteor.subscribe("userData");
+
+    // Listen for unique loggers and store them in session variable
+    // so that they can be used in the template later on
+    Session.setDefault('loggers',[]);
+    Session.set('autoScroll', true);
+    
+    Logs.find().observeChanges({
+        added: function (id, doc) {
+            if (loggers[doc.logger]) {
+                loggers[doc.logger]++;
+            }
+            else {
+                loggers[doc.logger]=1;
+                Session.set('loggers', Object.keys(loggers).sort());
+            }
+        },
+        removed: function (id) {
+            /*
+            console.log(id, doc);
+            loggers[doc.logger]--;
+            if (loggers[doc.logger]==0) {
+                delete loggers[doc.logger];
+                Session.set('loggers', Object.keys(loggers).sort());
+            }
+            */
+        }
+    });
 });
